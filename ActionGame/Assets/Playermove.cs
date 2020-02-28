c9023f4fd833d9d1c3f00fb0628c6f1ec2242070
tanakasotaro <strunv0302@gmail.com>
2020-02-28 19:32:51 +0900

    newgame

---
+++ ActionGame/Assets/Playermove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermove : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.UpArrow)){
+            rb.AddForce(0,0,5);
+        }
+
+        if(Input.GetKey(KeyCode.DownArrow)){
+            rb.AddForce(0,0,-5);
+        }
+
+        if(Input.GetKey(KeyCode.RightArrow)){
+            rb.AddForce(5,0,0);
+        }
+
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            rb.AddForce(-5,0,0);
+        }
+
+        if(transform.position.y < -10){
+            transform.position = new Vector3(0,4,0);
+        }
+    }
+
+    void OnCollisionStay(Collision hit){
+        if(hit.gameObject.tag == "Enemy"){
+            transform.position = new Vector3(0,4,0);
+        }
+    }
+}
